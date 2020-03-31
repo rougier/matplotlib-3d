b5c3eba23af2c30bebfc5d03c5db6fd63eba4168
Tetsuo Koyama <tkoyama010@gmail.com>
2020-03-31 18:08:55 +0900

    :package: for pip

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="mpl3d", version="0.0.1", install_requires=["matplotlib", "numpy"],
+    name="mpl3d",
+    version="0.0.1",
+    install_requires=["matplotlib", "numpy"],
+    packages=["mpl3d"],
 )
